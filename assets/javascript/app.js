99ab1d1cefa873777b401ef6f745ec7764e08eb8
adriansg85 <adriansalinasg@gmail.com>
2018-09-23 23:27:26 -0500

    advance 1

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+//Wait for document to load
+$(document).ready(function () {
+    //Create array to store the topics from the form
+    var topics = [];
+
+    $("#start-button").on("click", function () {
+        $("#button-area").fadeIn(3000);
+
+        $("#form-area").fadeIn(3000);
+
+        $("#display-area").fadeIn(3000);
+
+    })
+
+
+
+});
